59e09d2fbf63908f088ac0b88e42e7788ae3997b
Michael-ivyTech <mbaker423@ivytech.edu>
2025-09-02 09:31:40 -0400

    Created ifelseM02.py
    Added python file

---
+++ ifelseM02.py
@@ -0,0 +1,42 @@
+'''
+Michael
+ifelseM02.py
+
+This program accepts a student's first name and GPAs, then tests whether the student qualifies for the Dean's List or Honor Roll. This is done by using a while loop that continuously asks for the name and GPA until the user inputs 'ZZZ'.
+
+'''
+
+        
+while (True):
+    name = input("What is the student's first name? ")
+    
+    if (name == "ZZZ"): 
+        break
+
+    gpa = input("What is " + name + "'s GPA? ")
+    gpa = float(gpa)
+    stu_roll = None
+    
+    if (gpa >= 3.5):
+        stu_roll = "Dean's List"
+    elif (gpa >= 3.25):
+        stu_roll = "Honor Roll"
+    else:
+        stu_roll = "neither the Dean's List or Honor Roll."
+    
+    print(name + " with a GPA of " + str(gpa) + ", qualifies for " + stu_roll)
+
+'''
+John
+4.0
+Jack
+2.0
+Will
+3.0
+Jenna
+3.33
+Gabe
+4.45
+ZZZ
+
+'''
