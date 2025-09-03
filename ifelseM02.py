75dd3c4fe06924d68a6cae8121488b9fad8b0659
Michael-ivyTech <mbaker423@ivytech.edu>
2025-09-03 09:39:56 -0400

    Update ifelseM02.py
    I added the variable last_name to conform with requirements that I had missed. As prompting the user for the students last name was something that I over looked.

--- ifelseM02.py
+++ ifelseM02.py
@@ -2,18 +2,19 @@
 Michael Baker
 ifelseM02.py
 
-This program accepts a student's first name and GPAs, then tests whether the student qualifies for the Dean's List or Honor Roll. This is done by using a while loop that continuously asks for the name and GPA until the user inputs 'ZZZ'.
+This program accepts a student's first and last name and GPAs, then tests whether the student qualifies for the Dean's List or Honor Roll. This is done by using a while loop that continuously asks for the last name, first name, and GPA until the user inputs 'ZZZ' for the last name.
 
 '''
 
         
 while (True):
-    name = input("What is the student's first name? ")
+    last_name = input("What is the student's last name? ")
     
-    if (name == "ZZZ"): 
+    if (last_name == "ZZZ"): 
+        
         break
-
-    gpa = input("What is " + name + "'s GPA? ")
+    first_name = input("What is the student's first name? ")
+    gpa = input("What is " + first_name + "'s GPA? ")
     gpa = float(gpa)
     if (gpa < 0):
         print("Please enter a valid GPA.")
@@ -31,20 +32,26 @@ while (True):
     else:
         stu_roll = "neither the Dean's List or Honor Roll."
     
-    print(name + " with a GPA of " + str(gpa) + ", qualifies for " + stu_roll)
+    print(first_name + " " + last_name + " with a GPA of " + str(gpa) + ", qualifies for " + stu_roll)
 
 '''
 Test Input:
+Will
 John
 4.0
+Orta
 Jack
 2.0
+Baker
 Will
 3.0
+VanGor
 Jenna
 3.33
 Carl
+Carl
 -0.66
+Layman
 Gabe
 5.45
 ZZZ
